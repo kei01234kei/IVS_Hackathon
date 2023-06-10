5feb1118b9e4f9127cee70defea4fc120fbc9d14
mkXultra <eeapprenctice@gmail.com>
2023-06-10 15:21:52 +0900

    feat: add types

--- types/submission.ts
+++ types/submission.ts
@@ -1,3 +1,13 @@
+// example SubmissionResponse
+// {
+//     "id": "新規提出ID",
+//     "user_id": "ユーザーID",
+//     "problem_id": "問題ID",
+//     "problem_type": "問題タイプ",
+//     "content": "提出内容",
+//     "score": "スコア",
+//     "submitted_at": "提出時間"
+// }
 export interface SubmissionResponse {
   id: number;
   user_id: number;
---
+++ types/user.ts
@@ -0,0 +1,38 @@
+// example CreateUserResponse
+// {
+//     "id": "ユーザid (uid)"
+//     "username": "ユーザー名"
+// }
+export interface CreateUserResponse {
+  id: number;
+  username: string;
+}
+
+// example GetUerResponse
+// {
+//     "id": "ユーザid (uid)"
+//     "username": "ユーザー名"
+// }
+export interface GetUserResponse {
+  id: number;
+  username: string;
+}
+
+
+// example UpdateUerResponse
+// {
+//     "id": "ユーザid (uid)"
+//     "username": "ユーザー名"
+// }
+export interface UpdateUerResponse {
+  id: number;
+  username: string;
+}
+
+// example DeleteUerResponse
+// {
+//     "code": 0
+// }
+export interface DeleteUerResponse {
+  code: number;
+}
