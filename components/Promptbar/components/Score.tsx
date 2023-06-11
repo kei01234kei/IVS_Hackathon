5cf92f89312a1f7c4534a8ab4e44d0d1a235f9a1
Harxxki <haruki4dev@gmail.com>
2023-06-11 21:14:25 +0900

    update confetti

--- components/Promptbar/components/Score.tsx
+++ components/Promptbar/components/Score.tsx
@@ -1,19 +1,14 @@
+import { FC } from 'react';
+
 interface Props {
   maxScore: number;
   score: number;
 }
 
-export const Score = (props: Props) => {
-  const { maxScore, score } = props;
+export const Score: FC<Props> = ({ maxScore, score }) => {
   return (
     <div className="flex justify-center w-full">
       <div className="p-4 pb-7 flex flex-col gap-2 w-full">
-        {/* <div className="p-1 flex flex-col gap-2">
-          <p className="font-medium text-lg">スコア:</p>
-          <p className="font-medium text-4xl">
-            {`${score}`}/{`${maxScore}`}pt
-          </p>
-        </div> */}
         <div className="flex items-end space-x-6">
           <p className="font-medium text-base">スコア:</p>
           <p className="font-medium text-5xl">{`${score}`}</p>
