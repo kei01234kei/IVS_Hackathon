19cf53d57f811f9c11fcc6290f246ef820622820
Keisuke Ishigami <keisuke.130@icloud.com>
2023-06-11 15:40:30 +0900

    feat: 提出テスト用のエンドポイントを作りました

--- test_script.sh
+++ test_script.sh
@@ -64,3 +64,24 @@ curl -X POST 'http://localhost:3000/api/competitions/1/submissions' \
       }
     ]
 }'
+
+# gradedMultipleCaseUsingChatGPT problem type の問題に対するテスト用の curl コマンドです
+# 採点結果を submissions.json には出力しません
+curl -X POST 'http://localhost:3000/api/competitions/1/test' \
+-H 'Content-Type: application/json' \
+-d '{
+    "user_id": "1",
+    "problem_id": "3",
+    "messages": [
+      {
+        "message_id": "1",
+        "role": "system",
+        "content": "You are a smart assistant."
+      },
+      {
+        "message_id": "2",
+        "role": "user",
+        "content": "Given a document from a user, try to extract the following metadata:\n - minimum_monthly_salary: number or null\n - maximum_monthly_salary: number or null\n\nexample input1: 年棒240万円以上\nexample output1: {minimum_monthly_salary: 200000, maximum_monthly_salary:null}\n\n Respond with a JSON containing the extracted metadata in key value pairs. If you dont find a metadata field, dont specify it.\n ONLY JSON is accepted as a response."
+      }
+    ]
+}'
