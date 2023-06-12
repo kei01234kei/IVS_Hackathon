9406a502a412ba3b3df692a6bba6e435e6b2e90f
Keisuke Ishigami <keisuke.130@icloud.com>
2023-06-12 18:26:04 +0900

    feat: テスト用の curl コマンド一覧に calculate エンドポイントを追加しました

--- test_script.sh
+++ test_script.sh
@@ -88,23 +88,28 @@ curl -X POST 'http://localhost:3000/api/competitions/1/submissions' \
     }
 }'
 
-# # gradedMultipleCaseUsingChatGPT problem type の問題に対するテスト用の curl コマンドです
-# # 採点結果を submissions.json には出力しません
-# curl -X POST 'http://localhost:3000/api/competitions/1/calculation' \
-# -H 'Content-Type: application/json' \
-# -d '{
-#     "user_id": "1",
-#     "problem_id": "3",
-#     "messages": [
-#       {
-#         "message_id": "1",
-#         "role": "system",
-#         "content": "You are a smart assistant."
-#       },
-#       {
-#         "message_id": "2",
-#         "role": "user",
-#         "content": "Given a document from a user, try to extract the following metadata:\n - minimum_monthly_salary: number or null\n - maximum_monthly_salary: number or null\n\nexample input1: 年棒240万円以上\nexample output1: {minimum_monthly_salary: 200000, maximum_monthly_salary:null}\n\n Respond with a JSON containing the extracted metadata in key value pairs. If you dont find a metadata field, dont specify it.\n ONLY JSON is accepted as a response."
-#       }
-#     ]
-# }'
+# gradedMultipleCaseUsingChatGPT problem type の問題に対するテスト用の curl コマンドです
+# 採点結果を submissions.json には出力しません
+curl -X POST 'http://localhost:3000/api/competitions/1/calculation' \
+-H 'Content-Type: application/json' \
+-d '{
+    "user_id": "1",
+    "competition_id": 1,
+    "problem_id": 3,
+    "content": {
+      "id": "1",
+      "name": "sample",
+      "messages": [
+        {"role": "user", "content": "Given a document from a user, try to extract the following metadata:\n - minimum_monthly_salary: number or null\n - maximum_monthly_salary: number or null\n\nexample input1: 年棒240万円以上\nexample output1: {minimum_monthly_salary: 200000, maximum_monthly_salary:null}\n\n Respond with a JSON containing the extracted metadata in key value pairs. If you dont find a metadata field, dont specify it.\n ONLY JSON is accepted as a response."}
+      ],
+      "model": {
+        "id": "1",
+        "name": "gpt-3.5-turbo",
+        "maxLength": 1000,
+        "tokenLimit": 2000
+      },
+      "prompt": "あなたは優れたアシスタントです",
+      "temperature": 0,
+      "folderId": "1"
+    }
+}'
