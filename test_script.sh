f64710556a44ad5735bc5738c99261da88ac33a3
Keisuke Ishigami <keisuke.130@icloud.com>
2023-06-12 18:21:07 +0900

    テスト用の curl コマンドを書き換えました

--- test_script.sh
+++ test_script.sh
@@ -5,14 +5,18 @@ curl -X POST 'http://localhost:3000/api/competitions/1/submissions' \
 -H 'Content-Type: application/json' \
 -d '{
     "user_id": "1",
-    "problem_id": "1",
-    "messages": [
-        {
-            "message_id": "1",
-            "role": "user",
-            "content": "2"
-        }
-    ]
+    "competition_id": 1,
+    "problem_id": 1,
+    "content": {
+      "id": "1",
+      "name": "sample",
+      "messages": [
+        {"role": "user", "content": "2"}
+      ],
+      "prompt": "あなたは優れたアシスタントです",
+      "temperature": 0,
+      "folderId": "1"
+    }
 }'
 
 # 間違った回答を提出します
@@ -20,14 +24,18 @@ curl -X POST 'http://localhost:3000/api/competitions/1/submissions' \
 -H 'Content-Type: application/json' \
 -d '{
     "user_id": "1",
-    "problem_id": "1",
-    "messages": [
-        {
-            "message_id": "1",
-            "role": "user",
-            "content": "wrong answer"
-        }
-    ]
+    "competition_id": 1,
+    "problem_id": 1,
+    "content": {
+      "id": "1",
+      "name": "sample",
+      "messages": [
+        {"role": "user", "content": "wrong answer"}
+      ],
+      "prompt": "あなたは優れたアシスタントです",
+      "temperature": 0,
+      "folderId": "1"
+    }
 }'
 
 # gradedOneCaseByChatGPT problem type の問題に対する挙動確認用の curl コマンドです
@@ -35,14 +43,24 @@ curl -X POST 'http://localhost:3000/api/competitions/1/submissions' \
 -H 'Content-Type: application/json' \
 -d '{
     "user_id": "1",
-    "problem_id": "2",
-    "messages": [
-        {
-            "message_id": "1",
-            "role": "user",
-            "content": "二郎系ラーメン屋の店長さんみたいですね。"
-        }
-    ]
+    "competition_id": 1,
+    "problem_id": 2,
+    "content": {
+      "id": "1",
+      "name": "sample",
+      "messages": [
+        {"role": "user", "content": "二郎系ラーメン屋の店長さんみたいですね。"}
+      ],
+      "model": {
+        "id": "1",
+        "name": "gpt-3.5-turbo",
+        "maxLength": 1000,
+        "tokenLimit": 2000
+      },
+      "prompt": "あなたは優れたアシスタントです",
+      "temperature": 0,
+      "folderId": "1"
+    }
 }'
 
 # gradedMultipleCaseUsingChatGPT problem type の問題に対する挙動確認用の curl コマンドです
@@ -50,38 +68,43 @@ curl -X POST 'http://localhost:3000/api/competitions/1/submissions' \
 -H 'Content-Type: application/json' \
 -d '{
     "user_id": "1",
-    "problem_id": "3",
-    "messages": [
-      {
-        "message_id": "1",
-        "role": "system",
-        "content": "You are a smart assistant."
+    "competition_id": 1,
+    "problem_id": 3,
+    "content": {
+      "id": "1",
+      "name": "sample",
+      "messages": [
+        {"role": "user", "content": "Given a document from a user, try to extract the following metadata:\n - minimum_monthly_salary: number or null\n - maximum_monthly_salary: number or null\n\nexample input1: 年棒240万円以上\nexample output1: {minimum_monthly_salary: 200000, maximum_monthly_salary:null}\n\n Respond with a JSON containing the extracted metadata in key value pairs. If you dont find a metadata field, dont specify it.\n ONLY JSON is accepted as a response."}
+      ],
+      "model": {
+        "id": "1",
+        "name": "gpt-3.5-turbo",
+        "maxLength": 1000,
+        "tokenLimit": 2000
       },
-      {
-        "message_id": "2",
-        "role": "user",
-        "content": "Given a document from a user, try to extract the following metadata:\n - minimum_monthly_salary: number or null\n - maximum_monthly_salary: number or null\n\nexample input1: 年棒240万円以上\nexample output1: {minimum_monthly_salary: 200000, maximum_monthly_salary:null}\n\n Respond with a JSON containing the extracted metadata in key value pairs. If you dont find a metadata field, dont specify it.\n ONLY JSON is accepted as a response."
-      }
-    ]
+      "prompt": "あなたは優れたアシスタントです",
+      "temperature": 0,
+      "folderId": "1"
+    }
 }'
 
-# gradedMultipleCaseUsingChatGPT problem type の問題に対するテスト用の curl コマンドです
-# 採点結果を submissions.json には出力しません
-curl -X POST 'http://localhost:3000/api/competitions/1/test' \
--H 'Content-Type: application/json' \
--d '{
-    "user_id": "1",
-    "problem_id": "3",
-    "messages": [
-      {
-        "message_id": "1",
-        "role": "system",
-        "content": "You are a smart assistant."
-      },
-      {
-        "message_id": "2",
-        "role": "user",
-        "content": "Given a document from a user, try to extract the following metadata:\n - minimum_monthly_salary: number or null\n - maximum_monthly_salary: number or null\n\nexample input1: 年棒240万円以上\nexample output1: {minimum_monthly_salary: 200000, maximum_monthly_salary:null}\n\n Respond with a JSON containing the extracted metadata in key value pairs. If you dont find a metadata field, dont specify it.\n ONLY JSON is accepted as a response."
-      }
-    ]
-}'
+# # gradedMultipleCaseUsingChatGPT problem type の問題に対するテスト用の curl コマンドです
+# # 採点結果を submissions.json には出力しません
+# curl -X POST 'http://localhost:3000/api/competitions/1/calculation' \
+# -H 'Content-Type: application/json' \
+# -d '{
+#     "user_id": "1",
+#     "problem_id": "3",
+#     "messages": [
+#       {
+#         "message_id": "1",
+#         "role": "system",
+#         "content": "You are a smart assistant."
+#       },
+#       {
+#         "message_id": "2",
+#         "role": "user",
+#         "content": "Given a document from a user, try to extract the following metadata:\n - minimum_monthly_salary: number or null\n - maximum_monthly_salary: number or null\n\nexample input1: 年棒240万円以上\nexample output1: {minimum_monthly_salary: 200000, maximum_monthly_salary:null}\n\n Respond with a JSON containing the extracted metadata in key value pairs. If you dont find a metadata field, dont specify it.\n ONLY JSON is accepted as a response."
+#       }
+#     ]
+# }'
