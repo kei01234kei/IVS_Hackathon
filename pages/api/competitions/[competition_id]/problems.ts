5e27f4d4433d9afe51227ada9f3b897c9a876680
KitaharaMugiro <you@example.com>
2023-06-13 20:56:09 +0900

    謎の405エラー起きるのでメソッドチェック外す

--- pages/api/competitions/[competition_id]/problems.ts
+++ pages/api/competitions/[competition_id]/problems.ts
@@ -10,25 +10,24 @@ const filePaths = {
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // HTTPメソッドをチェックします。
-  if (req.method === 'GET') {
-    const { competition_id } = req.query;
 
-    // JSONファイルから問題を読み込む
-    let problems: any;
-    try {
-      problems = JSON.parse(fs.readFileSync(filePaths.problems, 'utf8'));
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
+  const { competition_id } = req.query;
 
-    // 問題と一覧を見つける
-    const responseProblems = problems.filter((p: any) => String(p.competition_id) === String(competition_id)) as GetProblemResponse[];
-    if (!responseProblems) {
-      throw new Error(`Problem not found with competition_id: ${competition_id}`);
-    }
-
-    // レスポンスとして問題を返します。
-    res.status(200).json({ problems: responseProblems });
+  // JSONファイルから問題を読み込む
+  let problems: any;
+  try {
+    problems = JSON.parse(fs.readFileSync(filePaths.problems, 'utf8'));
+  } catch (error) {
+    res.status(500).json({ error: error });
   }
+
+  // 問題と一覧を見つける
+  const responseProblems = problems.filter((p: any) => String(p.competition_id) === String(competition_id)) as GetProblemResponse[];
+  if (!responseProblems) {
+    throw new Error(`Problem not found with competition_id: ${competition_id}`);
+  }
+
+  // レスポンスとして問題を返します。
+  res.status(200).json({ problems: responseProblems });
+
 }
