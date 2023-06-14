56a561eb919b502fb55c4bbd7db83d78be16993e
mkXultra <34665721+mkXultra@users.noreply.github.com>
2023-06-14 22:15:18 +0900

    fix: show next line (#37)

--- components/Chatbar/components/ProblemDescription.tsx
+++ components/Chatbar/components/ProblemDescription.tsx
@@ -40,11 +40,11 @@ export const ProblemDescription = (props: Props) => {
               <LevelBadge level={problem.level} />
             </div>
           </div>
-          <div className="p-1 flex flex-col gap-2">
+          <div className="p-1 flex flex-col gap-2 whitespace-pre-wrap">
             <p className="font-semibold text-xl">問題文</p>
             <p className="font-medium text-sm">{problem.content}</p>
           </div>
-          <div className="p-1 flex flex-col gap-2">
+          <div className="p-1 flex flex-col gap-2 whitespace-pre-wrap">
             <p className="font-semibold text-xl">出力フォーマット</p>
             <p className="font-medium text-sm">{problem.output_example}</p>
           </div>
